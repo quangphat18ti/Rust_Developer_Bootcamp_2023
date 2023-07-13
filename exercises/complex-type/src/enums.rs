79f5bb295696d29c400430e1f5fce82c7716b626
quangphat18ti <quangphat18ti@gmail.com>
2023-07-13 18:58:36 +0700

    homework-3

--- exercises/complex-type/src/enums.rs
+++ exercises/complex-type/src/enums.rs
@@ -1,6 +1,7 @@
 // Exercise 1
 // Fill in the blank and fix the errors
 // Make it compile
+#[derive(Debug)]
 enum MessageOne {
     Quit,
     Move { x: i32, y: i32 },
@@ -8,11 +9,11 @@ enum MessageOne {
     ChangeColor(i32, i32, i32),
 }
 fn show_message(msg: MessageOne) {
-    println!("{}", msg);
+    println!("{:?}", msg);
 }
 
 fn exercise1() {
-    let msgs: __ = [
+    let msgs:[MessageOne; 3] = [
         MessageOne::Quit,
         MessageOne::Move { x: 1, y: 3 },
         MessageOne::ChangeColor(255, 255, 0),
@@ -29,6 +30,10 @@ fn exercise1() {
 // Run tests
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit
 }
 
 struct Point {
@@ -62,6 +67,12 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor(r, g, b) => self.change_color((r,g,b)),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit()
+        }
     }
 }
 
@@ -69,6 +80,7 @@ impl State {
 // Exercise 3
 // Fix the errors
 // Run tests
+#[derive(PartialEq, Debug)]
 enum Direction {
     North,
     East,
@@ -78,8 +90,12 @@ enum Direction {
 
 impl Direction {
     fn opposite(&self) -> Direction {
-        match self {
+        match (*self) {
             //TODO
+            Direction::East => Direction::West,
+            Direction::North => Direction::South,
+            Direction::West => Direction::East,
+            Direction::South => Direction::North
         }
     }
 }
@@ -99,7 +115,10 @@ enum Operation {
 // Perform arithmetic operations
 fn perform_operation(operation: Operation, num1: f64, num2: f64) -> f64 {
     match operation {
-        // TODO
+        Operation::Add => num1 + num2,
+        Operation::Subtract => num1 - num2,
+        Operation::Divide => num1 / num2,
+        Operation::Multiply => num1 * num2,
     }
 }
 
--- exercises/complex-type/src/structs.rs
+++ exercises/complex-type/src/structs.rs
@@ -2,6 +2,8 @@
 // Fix the error
 // Make it compile
 // Run test
+// Để Các Struct tự động có thể so sánh == thì ta cần Derive PartialEq
+#[derive(Debug, PartialEq)]
 struct Person {
     name: String,
     age: u8,
@@ -39,12 +41,12 @@ impl Agent {
 
     // Get the name of the person
     fn get_name(&self) -> &str {
-        todo!()
+        &self.name
     }
 
     // Get the age of the person
     fn get_age(&self) -> u32 {
-        todo!()
+            self.age
     }
 }
 
@@ -61,25 +63,25 @@ impl Calculator {
         Calculator { value: 0 }
     }
 
-    fn add(&self, num: i32) {
+    fn add(&mut self, num: i32) {
         self.value += num;
     }
 
-    fn subtract(mut self, num: i32) {
+    fn subtract(&mut self, num: i32) {
         self.value -= num;
     }
-    fn clear(self) {
+    fn clear(&mut self) {
         self.value = 0;
     }
 
-    fn get_value(self) -> i32 {
+    fn get_value(&self) -> i32 {
         self.value
     }
 }
 
 // Exercise 4
 // Make it compile
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct User {
     first: String,
     last: String,
@@ -95,8 +97,7 @@ fn exercise4() {
 
     let u2 = User {
         first: String::from("Mary"),
-        ..u1
-        
+        ..u1.clone()
     };
 
     println!("user: {:#?}", u1);
@@ -116,16 +117,17 @@ fn exercise5() {
         str_val: "ten".to_string(),
         int_val: 10,
     });
+
     foos.push(Foo {
         str_val: "twenty".to_string(),
         int_val: 20,
     });
 
     
-    let moved = foos[0];
+    let moved = &foos[0];
 
     
-    let moved_field = foos[0].str_val;
+    let moved_field = &foos[0].str_val;
 }
 
 // Exercise 6
@@ -153,12 +155,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        cents_per_gram * self.weight_in_grams
     }
 }
 
